4145ee6a1606e6f74e4705054f87958c87419974
KCMicheal <egwunwokekc@gmail.com>
2022-02-02 16:07:00 +0100

    perf: updated interface

--- Repository/IUserRepository.cs
+++ Repository/IUserRepository.cs
@@ -1,12 +1,15 @@
+using System.Collections;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using census.Entities.Models;
 
 namespace census.Repository
 {
     public interface IUserRepository
     {
-        
+        IEnumerable<UserModel> GetAll();
+        UserModel Add(UserModel user);
     }
 }
